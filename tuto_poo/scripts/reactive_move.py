84723f54b4d87e0beae31a3b336621d743549bac
bot <mobisyst@imt-nord-europe.fr>
2023-12-22 14:21:16 +0100

    test commit

--- tuto_poo/scripts/reactive_move.py
+++ tuto_poo/scripts/reactive_move.py
@@ -6,15 +6,18 @@ from sensor_msgs.msg import LaserScan
 from std_msgs.msg import Header
 import sensor_msgs.msg._point_cloud2 as pc2
 import sensor_msgs_py.point_cloud2
+# test
+
 
 class reactive_node(Node):
 
     def __init__(self):
         super().__init__('reactive_node')
-        self.subscription= self.create_subscription(
+        self.subscription = self.create_subscription(
             LaserScan, 'scan',
             self.scan_callback, 10)
-        self.cloud_publisher = self.create_publisher(pc2.PointCloud2, 'laser_link', 10)
+        self.cloud_publisher = self.create_publisher(
+            pc2.PointCloud2, 'laser_link', 10)
 
     def scan_callback(self, scanMsg):
         obstacles = []
@@ -28,12 +31,14 @@ class reactive_node(Node):
                 obstacles.append(aPoint)
             angle += scanMsg.angle_increment
         sample = [[round(p[0], 2), round(p[1], 2), 0.0] for p in obstacles]
-        sampleCloud = sensor_msgs_py.point_cloud2.create_cloud_xyz32(Header(frame_id='laser_link'), sample)
+        sampleCloud = sensor_msgs_py.point_cloud2.create_cloud_xyz32(
+            Header(frame_id='laser_link'), sample)
 
         for point in sensor_msgs_py.point_cloud2.read_points(sampleCloud):
             print(point)
 
         self.cloud_publisher.publish(sampleCloud)
+
 
 def main(args=None):
     print("Node REACTIVE_MOVE started")
@@ -42,6 +47,7 @@ def main(args=None):
     rclpy.spin(reactive_node)
     reactive_node.destroy_node()
     rclpy.shutdown()
+
 
 if __name__ == '__main__':
     main()

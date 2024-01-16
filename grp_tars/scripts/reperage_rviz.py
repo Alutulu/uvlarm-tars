0ca67ef71f0505c38d1e46c2fb0d465f69eaf574
alutulu <arthur.mata24@gmail.com>
2024-01-16 16:15:38 +0100

    draft reperage

---
+++ grp_tars/scripts/reperage_rviz.py
@@ -0,0 +1,45 @@
+from geometry_msgs.msg import PointStamped
+import rclpy
+from rclpy.node import Node
+import time
+
+class MoveBasic(Node):
+
+    def __init__(self, node_name, topic_move_name = '/abracadabra'):
+        super().__init__(node_name)
+        self.i=0
+
+
+        self.position=PointStamped()
+        self.position.point.x=0.01
+        self.position.point.y=0.002
+        self.position.point.z=0.002
+        self.position.header.frame_id = 'map'
+        self.position.header.stamp = self.get_clock().now().to_msg()
+        self.pub = self.create_publisher(PointStamped, topic_move_name, 10)
+    
+    def publishh(self):
+        while True:
+            print(self.position.point.x,self.position.point.y)
+            self.pub.publish(self.position)
+            time.sleep(1)
+
+
+       
+
+
+def main(args=None):
+    print("Node DETECTION started")
+    isOk = True # Capture ctrl-c event
+    rclpy.init(args=args)
+    publishhh=MoveBasic("vbhjv")
+
+
+    while isOk:
+        publishhh.publishh()
+        
+        
+
+if __name__ == '__main__':
+    main()
+

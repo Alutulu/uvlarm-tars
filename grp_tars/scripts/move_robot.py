0c50aa11fe03d90bfee7cf9650d3d9de8e8cfc30
alutulu <arthur.mata24@gmail.com>
2024-01-16 11:38:23 +0100

    fix node name

--- grp_tars/scripts/move_robot.py
+++ grp_tars/scripts/move_robot.py
@@ -5,7 +5,7 @@ from move_basic import MoveBasic
 def main(args=None):
     print("Node MOVE_SIMULATION started")
     rclpy.init(args=args)
-    moveSimulationNode = MoveBasic('move_simulation_node', 0.6, 0.25, 3, 2, '/multi/cmd_val')
+    moveSimulationNode = MoveBasic('move_robot_node', 0.6, 0.25, 3, 2, '/multi/cmd_val')
     rclpy.spin(moveSimulationNode)
     moveSimulationNode.destroy_node()
     rclpy.shutdown()

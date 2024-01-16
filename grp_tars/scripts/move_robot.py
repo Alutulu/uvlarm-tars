d1efd2c336677d97b1643d1b7e33e9983244cf0d
bot <mobisyst@imt-nord-europe.fr>
2024-01-16 15:40:28 +0100

    stop when wheels droped

--- grp_tars/scripts/move_robot.py
+++ grp_tars/scripts/move_robot.py
@@ -2,13 +2,16 @@
 import rclpy
 from move_basic import MoveBasic
 
+
 def main(args=None):
     print("Node MOVE_SIMULATION started")
     rclpy.init(args=args)
-    moveSimulationNode = MoveBasic('move_robot_node', 0.6, 0.25, 3, 2, '/multi/cmd_val')
+    moveSimulationNode = MoveBasic(
+        'move_robot_node', 0.6, 0.25, 0.3, 0.5, '/multi/cmd_nav')
     rclpy.spin(moveSimulationNode)
     moveSimulationNode.destroy_node()
     rclpy.shutdown()
+
 
 if __name__ == '__main__':
     main()

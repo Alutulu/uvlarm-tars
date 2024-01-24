2ffda18e9a63a84158711ccfc2bc0da9eb35d639
Alutulu <arthur.mata24@gmail.com>
2024-01-24 23:32:27 +0100

    clean vision

--- tuto_poo/scripts/MSContext.py
+++ tuto_poo/scripts/MSContext.py
@@ -2,23 +2,23 @@
 import rclpy
 from rclpy.node import Node
 
-# class MSContext(Node):
+class MSContext(Node):
 
-#     def listener_callback(self, msg):
-#         self.get_logger().info('I heard: "%s"' % msg.data)
+    def listener_callback(self, msg):
+        self.get_logger().info('I heard: "%s"' % msg.data)
 
-#     def process(self, args):
-#         rclpy.init(args=args)
-#         self._node = Node()
-#         self._node.create_subscription(
-#             String, 'topic',
-#             self.listener_callback, 10)
-#         # Infinite loop:
-#         rclpy.spin(minimal_subscriber)
-#         # Clean stop:
-#         minimal_subscriber.destroy_node()
-#         rclpy.shutdown()
+    def process(self, args):
+        rclpy.init(args=args)
+        self._node = Node()
+        self._node.create_subscription(
+            String, 'topic',
+            self.listener_callback, 10)
+        # Infinite loop:
+        rclpy.spin(minimal_subscriber)
+        # Clean stop:
+        minimal_subscriber.destroy_node()
+        rclpy.shutdown()
 
-# def main():
-#     minimal_subscriber= MSContext()
-#     minimal_subscriber.process()
+def main():
+    minimal_subscriber= MSContext()
+    minimal_subscriber.process()
